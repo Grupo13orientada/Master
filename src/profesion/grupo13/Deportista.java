625cff0078c9d449f165374eff85bfc335780bd5
mejia.mario0813@gmail.com <>
2021-07-14 22:19:48 -0600

    Yo, Mario Mejia programe la clase nombrada Deportista

---
+++ src/profesion/grupo13/Deportista.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package profesion.grupo13;
+import Persona.Persona;
+
+/**
+ *
+ * @author Mario Mejia
+ */
+public class Deportista extends Persona {
+    
+     public Deportista() {
+    
+    setNombre("Mario Mejia");
+        setEdad(20);
+        setTrabajo("Jugador Profecional");
+    
+}
+}
+
