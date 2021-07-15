639ad54b8db80db65b3c7fc8f473a5013e2224b4
grupo13orientada@gmail.com <>
2021-07-14 22:54:56 -0600

    Programa Completo. realizado por el grupo 13

---
+++ src/profesion/grupo13/Doctor.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package profesion.grupo13;
+import Persona.Persona;
+/**
+ *
+ * @author Issis Hernandez
+ */
+public class Doctor extends Persona {
+    
+    public Doctor() {
+        
+        setNombre("Issis Hernandez");
+        setEdad(23);
+        setTrabajo("Hospital Mario Catarino Rivas");
+    
+}
+    
+    
+    
+}
+
--- src/profesion/grupo13/ProfesionGrupo13.java
+++ src/profesion/grupo13/ProfesionGrupo13.java
@@ -15,7 +15,15 @@ public class ProfesionGrupo13 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Doctor d= new Doctor();
+        Deportista p= new Deportista();
+        
+        System.out.println("Individuo 1");
+        d.imprimirinformacion();
+       
+        
+        System.out.println("Individuo 2");
+        p.imprimirinformacion(); 
     }
     
 }
